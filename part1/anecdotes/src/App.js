794770357944290a31e0dc40cdad0a74c0213f7d
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-01-11 17:01:10 +0000

    add step 3 of the app anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+const Header = ({text}) => <h2>{text}</h2>
+
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
@@ -14,24 +16,44 @@ const App = () => {
   ];
   
   const [selected, setSelected] = useState(0);
-  const [points, setPoints] = useState({0:0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0});
+  const [points, setPoints] = useState([0, 0, 0, 0, 0, 0, 0]);
+  const [mostVotes, setMostVotes] = useState(0);
 
   // set a random anecdote from the array to be selected
   const drawAnecdote = () => setSelected(Math.floor(Math.random() * 7));
-  // update points for the displayed anecdote
+  // update points for the displayed anecdote and check which one has the most votes
   const vote = () => {
-    const copy = {...points};
+    const copy = [...points];
     copy[selected] += 1;
     setPoints(copy);
+    if (copy[selected] > points[mostVotes]){
+      setMostVotes(selected);
+    }
   }
+
+  /*    let index = 0;
+    let maxPoints = 0;
+    for(let i = 0; i < points.length; i++){
+      if(points[i] > maxPoints){
+        index = i;
+        maxPoints = points[i];
+      }
+    }
+    setMostVotes(index);
+    */
+
   
 
   return (
     <div>
+      <Header text={'Anecdote of the day'} />
       <p>{anecdotes[selected]}</p>
       <p>has {points[selected]} votes</p>
       <Button onClick={vote} text={'vote'}/>
       <Button onClick={drawAnecdote} text={'next anecdote'}/>
+      <Header text={'Anecdote with most votes'} />
+      <p>{anecdotes[mostVotes]}</p>
+      <p>has {points[mostVotes]} votes</p>
     </div>
   )
 };
