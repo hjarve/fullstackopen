4b2781cde1e93113944065cf39a93afe63322817
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-01-14 17:26:27 +0000

    fix a bug with filtering the names in app phonebook, component Persons

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -3,7 +3,7 @@ const Person = ({person}) => <p>{person.name} {person.number}</p>
 
 const Persons = ({persons, filterString}) => {
   //filter the people whose names contain the filterString, case insensitive
-  return (persons.filter(person => person.name.toLowerCase().includes(filterString)).map(person => {
+  return (persons.filter(person => person.name.toLowerCase().includes(filterString.toLowerCase())).map(person => {
      return <Person key={person.id} person={person}/>}
     )
   )
