cba43e4dcd01629f1c4fb7dceb4d210c5f13674c
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-04-08 19:24:16 +0900

    Write a test to verify that a blog can be added

--- part4/bloglist/tests/blog_list_api.test.js
+++ part4/bloglist/tests/blog_list_api.test.js
@@ -40,6 +40,27 @@ test('Unique identifier is named id', async () => {
     expect(response.body[0].id).toBeDefined();
 })
 
+test('a blog can be added', async () => {
+    const newBlog = {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(blog => blog.title)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(contents).toContain('Canonical string reduction')
+})
+
 afterAll(async () => {
     await mongoose.connection.close();
 }, 100000)
