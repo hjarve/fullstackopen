0c38dbce50f9cac4065bae02bca2da34e29ae4ae
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-04-19 21:46:12 +0900

    Add references of blogs and users to eachother's models, update get all to show the added information

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,24 +1,32 @@
 const blogsRouter = require('express').Router();
 const { response } = require('../app');
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 })
 
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body;
 
+    const users = await User.find({});
+    const user = users[0];
+    console.log(user);
+
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: body.likes || 0
+      likes: body.likes || 0,
+      user: user._id
     })
 
     try{
       const savedBlog = await blog.save();
+      user.blogs = user.blogs.concat(savedBlog._id);
+      await user.save();
     response.status(201).json(savedBlog);
     } catch(e){
       next(e);
