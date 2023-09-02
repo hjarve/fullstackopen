7da36254cfb1f86ed30eac53865940e5f63db017
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-09-02 17:27:04 +0300

    Add a cypress test for successful login.

--- part5/bloglist_frontend_vite/cypress/e2e/bloglist_app_.cy.js
+++ part5/bloglist_frontend_vite/cypress/e2e/bloglist_app_.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'User 1',
+      username: 'user1',
+      password: 'secretPass0rd'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user);
     cy.visit('http://localhost:5173')
   })
 
@@ -8,5 +14,21 @@ describe('Blog app', function() {
     cy.contains('log in to the application')
     cy.get('#username')
     cy.get('#password')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('user1')
+      cy.get('#password').type('secretPass0rd')
+      cy.get('#login-button').click()
+
+      cy.contains('User 1 logged in')
+    })
+
+    /*
+    it('fails with wrong credentials', function() {
+      // ...
+    })
+    */
   })
 })
