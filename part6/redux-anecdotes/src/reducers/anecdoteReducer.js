b2d5500f01c665157a34f3a8a8c0ace27827b8ce
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-09-05 15:03:43 +0300

    Implement the functionality for voting anecdotes.

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,22 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch(action.type){
+    case('VOTE'):
+      const id = action.payload.id
+      return state.map(anecdote => anecdote.id === id ? {...anecdote, votes: anecdote.votes + 1} : anecdote)
+
+    default: return state;
+  }
+}
+
+export const voteAnecdote = (id) => {
+  return{
+    type: 'VOTE',
+    payload: {
+      id: id
+    }
+  }
 }
 
 export default reducer
