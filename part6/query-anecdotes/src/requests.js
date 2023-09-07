469af1c1f514eb56c4ee68d5f48ae4b50ac1a618
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-09-07 14:31:51 +0300

    The anecdote must be at least 5 characthers long, otherwise it is rejected.

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -4,4 +4,6 @@ const baseUrl = 'http://localhost:3001/anecdotes';
 
 export const getAnecdotes = () => axios.get(baseUrl).then(res => res.data);
 
-export const createAnecdote = newAnecdote => axios.post(baseUrl, newAnecdote).then(res => res.data);
+export const createAnecdote = newAnecdote => {
+  if ( newAnecdote.content.length >= 5) axios.post(baseUrl, newAnecdote).then(res => res.data);
+} 
