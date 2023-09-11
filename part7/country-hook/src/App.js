27f8c1cff697c2900ebeaa8c9cf4323fdf283857
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-09-11 21:27:20 +0300

    Implement useCountry hook to fetch country's data.

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,20 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if(name){
+      axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        .then(response => {
+          setCountry({...response, found: true})
+        })
+        .catch(function (error){
+          console.log(error)
+          setCountry({found: false});
+        })
+    } else {
+      setCountry(null);
+    }
+  }, [name]);
 
   return country
 }
@@ -38,10 +51,10 @@ const Country = ({ country }) => {
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
+      <h3>{country.data.name.common} </h3>
       <div>capital {country.data.capital} </div>
       <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name}`}/>  
     </div>
   )
 }
