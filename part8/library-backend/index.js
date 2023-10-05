090a551ffc919ef0a4b15f736760897354ee3a29
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-10-05 13:50:46 +0300

    Implement allBooks query.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -93,9 +93,6 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
 
 const typeDefs = `
   type Author {
@@ -106,19 +103,22 @@ const typeDefs = `
   type Book {
     title: String!
     published: Int!
+    author: String!
     id: ID!
     genres: [String]
   }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books,
   }
 }
 
