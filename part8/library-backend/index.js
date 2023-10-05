2f2fda04eedd1f179f1cdeee9c8eadf3bc9db764
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-10-05 15:27:25 +0300

    Implement mutation editAuthor.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -122,6 +122,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -156,6 +160,13 @@ const resolvers = {
       const newBook = {...args, id: uuid()}
       books = books.concat(newBook)
       return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) return null
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
