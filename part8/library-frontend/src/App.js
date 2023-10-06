c0b3fae5f9e48227b01ba3b56295e4d70ec43b23
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-10-06 10:00:08 +0300

    Implement authors view to show the details of all authors.

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -2,9 +2,27 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
 
 const App = () => {
   const [page, setPage] = useState('authors')
+
+  const result = useQuery(ALL_AUTHORS)
+  
+  if (result.loading ) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -14,7 +32,7 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} />
+      <Authors show={page === 'authors'} authors={result.data.allAuthors}/>
 
       <Books show={page === 'books'} />
 
