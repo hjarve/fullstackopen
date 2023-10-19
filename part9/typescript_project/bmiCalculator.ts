60e93d07f8fc30e60a5cd7f21e136aee966ee5d1
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-10-19 20:25:26 +0300

    Add error handling to bmiCalculator and exerciseCalculator.

--- part9/typescript_project/bmiCalculator.ts
+++ part9/typescript_project/bmiCalculator.ts
@@ -1,5 +1,12 @@
 
 const calculateBmi = (heightInCm: number, weightInKg: number): string  => {
+  if ( isNaN(heightInCm) || isNaN(weightInKg)) {
+    throw new Error('Height and weight must be numbers!');
+  }
+  if ( heightInCm <= 0 || weightInKg <= 0 ) {
+    throw new Error('Height and weight must be greater than zero!')
+  }
+  
   const bmi = weightInKg/((heightInCm/100) ** 2)
   if ( bmi < 18.5 ){
     return 'undeweight'
@@ -15,4 +22,13 @@ const calculateBmi = (heightInCm: number, weightInKg: number): string  => {
 
 const heightInCm: number = Number(process.argv[2]);
 const weightInKg: number = Number(process.argv[3]);
-console.log(calculateBmi(heightInCm, weightInKg));
+
+try{
+  console.log(calculateBmi(heightInCm, weightInKg));
+} catch ( error: unknown) {
+  let errorMessage = 'There is an error: ';
+  if ( error instanceof Error ) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
--- part9/typescript_project/exerciseCalculator.ts
+++ part9/typescript_project/exerciseCalculator.ts
@@ -9,6 +9,19 @@ interface ReturnObj {
 }
 
 const calculateExercises = (dailyHours: number[], origTarget: number): ReturnObj => {
+  if ( isNaN(origTarget) || dailyHours.some((value) => isNaN(value))) {
+    throw new Error('All the parameters must be numbers!');
+  }
+  if (origTarget <= 0 ) {
+    throw new Error('Target must be greater than zero!');
+  }
+  if (!dailyHours.every((value) => value >= 0)){
+    throw new Error('Exercise hours must be greater than zero!');
+  }
+  if (dailyHours.length === 0 ) {
+    throw new Error('No daily hours provided.')
+  }
+  
   const periodLenth= dailyHours.length;
   let trainingDays = 0;
   dailyHours.map(d => d > 0 ? trainingDays+=1 : null)
@@ -45,4 +58,12 @@ const target: number = Number(process.argv[2])
 const daysString: string[] = process.argv.slice(3);
 const days: number[] = daysString.map(d => Number(d));
 
-console.log(calculateExercises(days, target));
+try {
+  console.log(calculateExercises(days, target));
+} catch ( error: unknown) {
+  let errorMessage = 'There is an error: ';
+  if ( error instanceof Error ) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
