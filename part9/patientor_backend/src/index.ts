9325fdf18d9a9690ce908ccc293ffd61362aae28
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-10-22 15:53:04 +0300

    Implement an endpoint /api/diagnoses for HTTP GET.

--- part9/patientor_backend/src/index.ts
+++ part9/patientor_backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosisRouter from './routes/diagnoses';
 import cors from 'cors';
 
 const app =express();
@@ -11,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('We got pinged');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
